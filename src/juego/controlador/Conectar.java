2aa01861b83c11dda4ab238f452747c59368230e
Miguel <Miguel@192.168.1.130>
2016-05-27 21:17:16 +0200

    Modificado Conectar

--- src/juego/controlador/Conectar.java
+++ src/juego/controlador/Conectar.java
@@ -12,7 +12,7 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 public class Conectar {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Connection connection = null;
     Statement statement = null;
     try {
@@ -39,6 +39,32 @@ public class Conectar {
         } // nothing we can do
       }
     }
+    creaTablas();
   }
+  public static void creaTablas () throws Exception {
+    String user = "root";
+    String pass = "";   
+    Class.forName("com.mysql.jdbc.Driver").newInstance();
+    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Miguel", user, pass);
+    Statement st = con.createStatement();
+    try{
+    String table = "CREATE TABLE java_DataTypes2(typ_boolean BOOL, "
+        + "typ_byte          TINYINT, typ_short         SMALLINT, "
+        + "typ_int           INTEGER, typ_long          BIGINT, "
+        + "typ_float         FLOAT,   typ_double        DOUBLE PRECISION, "
+        + "typ_bigdecimal    DECIMAL(13,0), typ_string        VARCHAR(254), "
+        + "typ_date          DATE,    typ_time          TIME, " + "typ_timestamp     TIMESTAMP, "
+        + "typ_asciistream   TEXT,    typ_binarystream  LONGBLOB, " + "typ_blob          BLOB)";
 
-}
+    st.executeUpdate(table);}
+    catch(Exception e)
+            {}
+    finally {
+      
+          st.close();
+
+          con.close();
+
+      }
+    }  
+  }
